4379bfae7a8806531622024d095d9ef401cc6cf2
Misato Miyahara <m.miyahara2046@gmail.com>
2020-03-17 10:12:09 +0900

    ブログ作成用.shファイル作成

---
+++ mkblog.sh
@@ -0,0 +1,43 @@
+# 入力
+number=$1
+title=$2
+description=$3
+
+date=`date '+%F'`
+today=`date "+%Y / %m / %d"`
+filePath="./pages/blogs/$date-$number.vue"
+jsonPath="./assets/json/blogs.json"
+
+# ファイル作成
+echo "ファイル作成 : $filePath"
+touch $filePath
+
+# 文字列追加
+echo ファイルにテンプレート文を追加
+echo -e "<template>
+  <div class=\"ly_blog\">
+    <header class=\"ly_header\">
+      <p class=\"el_blogDate\">
+        $today
+      </p>
+      <h1 class=\"el_blogHeader\">
+        $title
+      </h1>
+    </header>
+    <main class=\"ly_main\">
+      <p>
+        <span class=\"code\">text</span>
+      </p>
+      <pre class=\"code\">
+        &lt;code>
+      </pre>
+    </main>
+  </div>
+</template>
+
+<style lang=\"scss\" scoped>
+@import '~/assets/stylesheets/blog.scss';
+</style>" > "$filePath"
+
+# JSONに追加
+sed -i -e "4a {\n\t\t\t\"id\":\"2020-03-16-01\",\n\t\t\t\"title\":\"$title\",\n\t\t\t\"description\":\"$description\"\n\t\t}," $jsonPath
